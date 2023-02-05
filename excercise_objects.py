7037bcb67748e10a1c681ca8b054a006967596bd
Joshua Whittaker <whittaker.d.joshua@gmail.com>
2023-02-05 16:25:53 +0200

    Create workout, exercise, and set

--- excercise_objects.py
+++ excercise_objects.py
@@ -1,8 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import datetime
 
 
 @dataclass
 class Set:
+    number: int
     weight_kgs: float
     num_reps: int
 
@@ -10,11 +12,17 @@ class Set:
 @dataclass
 class Exercise:
     name: str
-    sets: list[Set]
+    sets: list[Set] = field(default_factory=list)
+
+    def add_set(self, set: Set):
+        self.sets.append(set)
 
 
 @dataclass
 class Workout:
-    date: str
+    date: datetime.date
     name: str
-    exercises: list[Exercise]
+    exercises: list[Exercise] = field(default_factory=list)
+
+    def add_exercise(self, exercise: Exercise):
+        self.exercises.append(exercise)
--- main.py
+++ main.py
@@ -1,48 +1,109 @@
-import sqlite3
 import datetime
+from collections import namedtuple
 from excercise_objects import Set, Exercise, Workout
 
 
-CURRENTLY_IMPLEMENTED = 1
-NUM_MENU_ITEMS = 7
+CURRENTLY_IMPLEMENTED = 1  # "Debug" value for me to change as I implement more features
+APP_OPTION = namedtuple("APP_OPTION", ["label", "function"])
 
 
 def init_db():
     pass
 
 
-def log_new_workout():
-    # date = datetime.date.today()
+def log_workout(workout: Workout):
     pass
 
 
-CHOICES = {
-    1: "Log a New Workout",
-    2: "View Previous Workouts",
-    3: "Log Meal",
-    4: "View Food Logs",
-    5: "Look up caloric value of excercise",
-    6: "Look up caloric value of food",
-    7: "Look up Supplement prices",
+def create_set(set_num: int):
+    set_weight = input("Weight: ")
+    set_num_reps = input("Number of reps: ")
+    set = Set(set_num, set_weight, set_num_reps)
+    return set
+
+
+def create_exercise() -> Exercise:
+    exc_name = input("Exercise name: ")
+    exc = Exercise(exc_name)
+    set_num = 1
+    while True:
+        set = create_set(set_num)
+        exc.add_set(set)
+        ans = input("Another set? (y/n): ")
+        if ans == "y":
+            set_num += 1
+        else:
+            break
+    return exc
+
+
+def create_workout() -> Workout:
+    workout_name = input("Workout name: ")
+    date = datetime.date.today()
+    workout = Workout(date, workout_name)
+    while True:
+        exc = create_exercise()
+        workout.add_exercise(exc)
+        ans = input("Another exercise? (y/n): ")
+        if ans == "n":
+            break
+    return workout
+
+
+def view_workouts():
+    pass
+
+
+def create_meal():
+    pass
+
+
+def view_meals():
+    pass
+
+
+def search_exercise():
+    pass
+
+
+def search_food():
+    pass
+
+
+def search_supplement():
+    pass
+
+
+OPTIONS = {
+    1: APP_OPTION("Log a New Workout", create_workout),
+    2: APP_OPTION("View Previous Workouts", view_workouts),
+    3: APP_OPTION("Log Meal", create_meal),
+    4: APP_OPTION("View Food Logs", view_meals),
+    5: APP_OPTION("Look up caloric value of excercise", search_exercise),
+    6: APP_OPTION("Look up caloric value of food", search_food),
+    7: APP_OPTION("Look up Supplement prices", search_supplement),
 }
+NUM_APP_OPTIONS = len(OPTIONS)
 
 
-def display_choices(choices: dict):
-    for choice, name in choices.items():
-        print(f"{choice}. {name}")
+def display_options(options: dict):
+    for number, option in options.items():
+        print(f"{number}. {option.label}")
 
 
 def main():
-    display_choices(CHOICES)
-    choice = input("Select an option by entering its number:")
+    display_options(OPTIONS)
+    option = input("Select an option by entering its number:")
 
     try:
-        choice = int(choice)
+        option = int(option)
+        OPTIONS.get(option).function()
+
     except ValueError:
         print(f"Invalid input. Please enter a valid number.")
-    if not (1 <= choice <= NUM_MENU_ITEMS):
+    if not (1 <= option <= NUM_APP_OPTIONS):
         raise IndexError
-    elif not (1 <= choice <= CURRENTLY_IMPLEMENTED):
+    elif not (1 <= option <= CURRENTLY_IMPLEMENTED):
         raise NotImplementedError
 
 
